5bfa9328c623d9b310ac701a9672a551fa40735c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:33 -0500

    xml commenting

--- src/Soenneker.Pinecone.HttpClients/Abstract/IPineconeOpenApiHttpClient.cs
+++ src/Soenneker.Pinecone.HttpClients/Abstract/IPineconeOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Pinecone.HttpClients.Abstract;
 /// </summary>
 public interface IPineconeOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
